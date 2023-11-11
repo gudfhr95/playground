86f9fc22539c3ceaa5af8e28106d05872c880f32
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-11-12 02:16:23 +0900

    chore: add hello-world app

---
+++ apps/hello-world/index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response, NextFunction} from 'express';
+
+// Constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Pulumi');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
