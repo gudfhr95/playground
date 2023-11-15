c25efb0cc9de63e6788ad6fe0909f78248008283
gudfhr95 <ghldnjsdlf@gmail.com>
2023-11-15 18:00:21 +0900

    chore: add dev, prod in hello-world

--- apps/hello-world/index.ts
+++ apps/hello-world/index.ts
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from hello-world');
+  res.send(`${process.env.NODE_ENV} hello-world`);
 });
 
 app.listen(PORT, HOST);
