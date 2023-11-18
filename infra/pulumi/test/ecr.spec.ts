ec1a4ae1c64ee9de44479f84c37ff0bd7555146d
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-11-19 01:35:26 +0900

    test: add simple s3 test

--- infra/pulumi/test/ecr.spec.ts
+++ infra/pulumi/test/ecr.spec.ts
@@ -1,7 +1,35 @@
-import {describe, expect, it} from '@jest/globals';
+import * as pulumi from '@pulumi/pulumi';
+import {beforeAll, beforeEach, describe, expect, it} from '@jest/globals';
+import {promiseOf} from './util';
 
-describe('My speaking clock', () => {
-  it('works', () => {
-    expect(true).toBeTruthy;
+describe('S3', () => {
+  let infra: typeof import('../src/ecr');
+
+  beforeAll(() => {
+    pulumi.runtime.setMocks({
+      newResource: (
+        args: pulumi.runtime.MockResourceArgs
+      ): {id: string; state: any} => {
+        return {
+          id: `${args.name}-id`,
+          state: args.inputs,
+        };
+      },
+
+      call: (args: pulumi.runtime.MockCallArgs) => {
+        return args.inputs;
+      },
+    });
+  });
+
+  beforeEach(async () => {
+    infra = await import('../src/ecr');
+  });
+
+  describe('bucket', () => {
+    it('should be created', async () => {
+      const bucketName = await promiseOf(infra.bucket.id);
+      expect(bucketName).toBe('my-bucket-3-id');
+    });
   });
 });
