dd933c70fedc03c7dee2f055c39bff91eefe58d7
gudfhr95 <ghldnjsdlf@gmail.com>
2023-11-20 15:13:02 +0900

    fix: add profile in ecr

--- infra/pulumi/src/ecr.ts
+++ infra/pulumi/src/ecr.ts
@@ -1,3 +1,8 @@
+import * as pulumi from '@pulumi/pulumi';
 import * as awsx from '@pulumi/awsx';
 
-export const helloWorldRepository = new awsx.ecr.Repository('hello-world', {});
+const config = new pulumi.Config();
+
+export const helloWorldRepository = new awsx.ecr.Repository('hello-world', {
+  name: `hello-world-${config.require('profile')}`,
+});
