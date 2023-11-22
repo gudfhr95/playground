c81589f886267479de7fb8fafabd83d0d0bc07d1
gudfhr95 <ghldnjsdlf@gmail.com>
2023-11-22 14:04:23 +0900

    test: release

--- infra/pulumi/src/ecr.ts
+++ infra/pulumi/src/ecr.ts
@@ -3,7 +3,6 @@ import * as awsx from '@pulumi/awsx';
 
 const config = new pulumi.Config();
 
-/* 1 */
 export const helloWorldRepository = new awsx.ecr.Repository('hello-world', {
   name: `hello-world-${config.require('profile')}`,
 });
