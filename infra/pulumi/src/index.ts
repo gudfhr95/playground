295d17952900ee769d4de340009e2ec12e0058b6
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-11-24 23:50:26 +0900

    feat: add eks

--- infra/pulumi/src/index.ts
+++ infra/pulumi/src/index.ts
@@ -1,3 +1,6 @@
 import {helloWorldRepository} from './ecr';
+import {cluster} from './eks';
+
+export const kubeconfig = cluster.kubeconfig;
 
 export const helloWorldRepositoryUrl = helloWorldRepository.url;
